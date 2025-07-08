22a87079fe280d8b8cdd00aeb5116d2a5f45a413
fanshicomic <fanshicomic@gmail.com>
2025-07-08 11:34:50 +0800

    feat: allow user to see group chat QR

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,1 +1,13 @@
-Component({})
+Component({
+  data: {
+    showModal: false
+  },
+  methods: {
+    showQRCode() {
+      this.setData({ showModal: true });
+    },
+    hideQRCode() {
+      this.setData({ showModal: false });
+    }
+  }
+})
