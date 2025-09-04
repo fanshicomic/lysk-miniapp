bdc9896b00fd7627432809797c13b726edac72e9
fanshicomic <fanshicomic@gmail.com>
2025-09-04 12:15:36 +0800

    feat: not display stage is no set card

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ let videoAd = null;
 Page({
   data: {
     title: '深空面板助手',
-    subTitle: 'Deepspace Battle Helper V3.0.0',
+    subTitle: 'Deepspace Battle Helper V3.0.3',
     blobs: [],
     showToast: false,
     userInfo: null,
